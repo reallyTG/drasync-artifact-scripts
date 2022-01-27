13c8b545f03de6306377c9ba1f59250b1c801b21
reallyTG <alexwithaneye@gmail.com>
2022-01-27 19:07:54 +0000

    convenience script to get dyn inv all patterns one project

---
+++ consolidate-dyn-inv-for-proj.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+projName=$1
+pattern=$2
+
+cd /home/evaluation/case-studies/$1
+
+find . -name '*.dynInv' | xargs -I {} ag 'asyncFunctionNoAwait' {} > $1-asyncFunctionNoAwait.txt
+find . -name '*.dynInv' | xargs -I {} ag 'awaitedReturnInAsyncFun' {} > $1-awaitedReturnInAsyncFun.txt
+find . -name '*.dynInv' | xargs -I {} ag 'awaitInLoop' {} > $1-awaitInLoop.txt
+find . -name '*.dynInv' | xargs -I {} ag 'explicitPromiseConstructor' {} > $1-explicitPromiseConstructor.txt
+find . -name '*.dynInv' | xargs -I {} ag 'inHousePromisification' {} > $1-inHousePromisification.txt
+find . -name '*.dynInv' | xargs -I {} ag 'promiseConstructorSyncResolve' {} > $1-promiseConstructorSyncResolve.txt
+find . -name '*.dynInv' | xargs -I {} ag 'promiseResolveThen' {} > $1-promiseResolveThen.txt
+find . -name '*.dynInv' | xargs -I {} ag 'reactionReturnsPromise' {} > $1-reactionReturnsPromise.txt
