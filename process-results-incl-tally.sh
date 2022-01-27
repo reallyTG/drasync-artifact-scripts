5ea1f48d1c7560a4f7d23316b545ae01bc04961b
Alexi Turcotte <alexi.s.f.turcotte@gmail.com>
2022-01-27 12:20:57 -0500

    oops

--- process-results-incl-tally.sh
+++ process-results-incl-tally.sh
@@ -7,7 +7,7 @@ cd /home/evaluation/case-studies/$projName
 rm processed-results-*
 
 ls results-* |
-xargs -I {} node /home/drasync/ProfilingPromisesProcessing/packageResultsToJSON.js --resFile {} --pathTo /home/evaluation/case-studies/$projName/ --antipatterns /data/evaluation/processed-query-results/$projName.csv 
+xargs -I {} node /home/drasync/ProfilingPromisesProcessing/packageResultsToJSON.js --resFile {} --pathTo /home/evaluation/case-studies/$projName/ --antipatterns /home/evaluation/processed-query-results/$projName.csv 
 
 # Once that's done, tally up.
 ls processed-results-* | 
